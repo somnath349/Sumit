ee9c7e334448c911faf6ff00aa0199e842f6c16c
Somnath Patel <60380177+somnath349@users.noreply.github.com>
2021-05-01 13:05:24 +0530

    Added a nested function

--- square.py
+++ square.py
@@ -3,3 +3,10 @@ l=[0,1,2,3,4,5]
 l1=list(map(lambda n:n*n,l))
 print(l1)
 Thank You
+Nested Functions
+def outer():
+  print("Outer function execution")
+  def inner():
+    print("Inner function execution")
+  inner()
+outer()  
